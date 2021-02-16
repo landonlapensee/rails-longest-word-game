f6ce1daa1c476ce016ee657ec9fcd4fd8a2ad962
Landon Lapensee <landonlapensee@gmail.com>
2021-02-15 20:44:29 -0800

    Half way through challenge

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def new
+    # counter = 0
+    # grid_size = rand
+    # @grid = []
+    # while counter < grid_size
+    #   grid << ('A'..'Z').to_a.sample
+    #   counter += 1
+    # end
+    # @grid
+  end
+
+  def score 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root to: 'pages#new'
+    get 'new', to: 'pages#new'
+    post 'score', to: 'pages#score'
+  # new GET  /new(.:format)   games#new
+  # score POST /score(.:format) games#score
 end
